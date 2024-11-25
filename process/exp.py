6e82137d4cc9a56c57789daba93f9b000dd4ecf9
liuzq <188005860@qq.com>
2024-11-25 14:26:12 +0800

    modify

--- process/exp.py
+++ process/exp.py
@@ -11,6 +11,56 @@ import torch.nn as nn
 import time
 
 
+def model_train(args, model, model_optim, criterion, train_loader, epochs):
+    train_steps = len(train_loader)
+    scheduler = lr_scheduler.OneCycleLR(optimizer=model_optim,
+                                        steps_per_epoch=train_steps,
+                                        pct_start=args.pct_start,
+                                        epochs=args.train_epochs,
+                                        max_lr=args.learning_rate)
+    if args.use_amp:
+        scaler = torch.cuda.amp.GradScaler()
+    early_stopping = EarlyStopping(patience=args.patience, verbose=True)
+    epochs = epochs//200
+    for ep in range(epochs+1):
+        train_loss = []
+        model.train()
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
+            model_optim.zero_grad()
+            batch_x = batch_x.float().to(args.device)
+
+            batch_y = batch_y.float().to(args.device)
+            if args.use_amp:
+                with torch.cuda.amp.autocast():
+                    outputs, res, trend = model(batch_x)
+            else:
+                outputs, res, trend = model(batch_x)
+            f_dim = -1 if args.features == 'MS' else 0
+            outputs = outputs[:, -args.pred_len:, f_dim:]
+            batch_y = batch_y[:, -args.pred_len:, f_dim:].to(args.device)
+            loss = criterion(outputs, batch_y)
+            # a = res.cpu().detach().numpy()
+            # b = trend.cpu().detach().numpy()
+            # cos_sim = cosine_similarity(a.reshape(1, -1), b.reshape(1, -1))
+            # print('cos_sim:', cos_sim)
+            train_loss.append(loss.item())
+
+            if args.use_amp:
+                scaler.scale(loss).backward()
+                scaler.step(model_optim)
+                scaler.update()
+            else:
+                loss.backward()
+                model_optim.step()
+
+            if args.lradj == 'TST':
+                adjust_learning_rate(model_optim, scheduler, ep+1, args, printout=False)
+                scheduler.step()
+
+        train_loss = np.average(train_loss)
+    return train_loss
+
+
 def evaluate_synset(net, x_sys, y_sys, train, vail_loader, test_loader, args):
     lr = float(args.lr_ts)
     Epoch = int(args.epoch_eval_train)
